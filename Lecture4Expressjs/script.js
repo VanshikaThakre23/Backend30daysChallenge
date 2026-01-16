469958b5aa407f48369e651e09e5dcdb619ce88b
Vanshika <vanshikathakre4@gmail.com>
2026-01-16 12:19:18 +0530

    express js intro and basic routing

---
+++ Lecture4Expressjs/script.js
@@ -0,0 +1,14 @@
+//intro to express and basic routing 
+
+const express = require ('express');
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("hello dear ");
+})
+
+app.get("/user",(req,res)=>{
+    res.send("hello i am user ");
+})
+
+app.listen(3000);
